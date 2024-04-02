1a5e28ff9503f8ab3986d32cb78c6e80783f5c3e
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2024-04-02 17:54:37 +0200

    Update defensive actions data file

--- limpieza_defensa.py
+++ limpieza_defensa.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import os
 
-goolkeeping = pd.read_csv(r'.\docs\data\PlayerStats_22_23\ucl_advanced_goalkeeping.csv')
+defenders = pd.read_csv(r'.\docs\data\PlayerStats_22_23\defensive_actions.csv')
 porteros_23_24 = ["Gianluigi Donnarumma", "Marc-André ter Stegen", "Jan Oblak", "Andriy Lunin", "Manuel Neuer", "Gregor Kobel", "Ederson"]
 
 
 # Si el portero no está en la lista de porteros, se elimina esa fila
-goolkeeping = goolkeeping[goolkeeping['Player'].isin(porteros_23_24)]
+defenders = defenders[defenders['Player'].isin(porteros_23_24)]
 # crear carpeta, si existe pasar
 os.makedirs(r'.\docs\data\clear', exist_ok=True)
-goolkeeping.to_csv(r'.\docs\data\clear\goalkeepers.csv', index=False)
+defenders.to_csv(r'.\docs\data\clear\defensive_actions.csv', index=False)
