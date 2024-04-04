aab249d87aad9f4992bb1a193ec806de281da340
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2024-04-04 16:31:09 +0200

    nada nuevo

--- limpieza_delanteros.py
+++ limpieza_delanteros.py
@@ -4,11 +4,16 @@ import os
 defenders = pd.read_csv(r'.\docs\data\PlayerStats_22_23\defensive_actions.csv')
 # Atleti
 # Real Madrid
-equipos = ['Real Madrid','Atletico','Paris S-G','Manchester City']
+defensas_23_24 = ["José María Giménez","Nahuel Molina","Mario Hermoso","Stefan Savić"
+                  "Éder Militão","Dani Carvajal","Ferland Mendy","Antonio Rüdiger"]
+
+#Equipos
+
+equipos = ["Atletico Madrid", "Real Madrid","Barcelona","Manchester City","Arsenal",]
 
 
 # Si el portero no está en la lista de defensas, se elimina esa fila
-defenders = defenders[defenders['Player'].isin(equipos)]
+defenders = defenders[defenders['Player'].isin(defensas_23_24)]
 # crear carpeta, si existe pasar
 os.makedirs(r'.\docs\data\clear', exist_ok=True)
 defenders.to_csv(r'.\docs\data\clear\defensive_actions.csv', index=False)
