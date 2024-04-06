12d6898925a44ac49500fbaf65b6ddc23a21fcd3
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2024-04-06 17:21:38 +0200

    funciona el script

--- limpieza_delanteros.py
+++ limpieza_delanteros.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import os
 
-df = pd.read_csv(r'.\docs\data\PlayerStats_22_23\ucl_shooting.csv')
+def limpieza(nombre_archivo:str):
+    df = pd.read_csv(r'.\docs\data\PlayerStats_22_23\{}.csv'.format(nombre_archivo))
 
-equipos = ['Real Madrid','Atletico','Paris S-G','Manchester City','Arsenal','Dortmund','Barcelona','Bayern Munich']
+    equipos = ['Real Madrid','Atletico','Paris S-G','Manchester City','Arsenal','Dortmund','Barcelona','Bayern Munich']
 
 
-df = df[df['Club'].isin(equipos)]
+    df = df[df['Club'].isin(equipos)]
 # crear carpeta, si existe pasar
-os.makedirs(r'.\docs\data\clear\strikers', exist_ok=True)
-df.to_csv(r'.\docs\data\clear\strikers\shooting.csv', index=False)
+    os.makedirs(r'.\docs\data\clear\strikers', exist_ok=True)
+    df.to_csv(r'.\docs\data\clear\strikers\shooting.csv', index=False)
+
+limpieza(input('Nombre del archivo: '))
