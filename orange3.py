9294f3534017fc17edaca9466118aa1bf0ce180c
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2024-04-10 23:36:09 +0200

    Delete shooting_goals.csv and orange3.py

--- orange3.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(r".\docs\data\clear\strikers\shooting.csv")
-
-df[df["Goals"]>0].to_csv(r".\docs\data\clear\strikers\shooting_goals.csv", index=False)
---
+++ probando_mongo.py
@@ -0,0 +1,20 @@
+from probando_mongo import MongoClient
+import datetime
+
+client = MongoClient('localhost', 27017)
+
+db = client['test-database']
+
+collection = db['test-collection']
+
+
+post = {
+    "author": "Mike",
+    "text": "My first blog post!",
+    "tags": ["mongodb", "python", "pymongo"],
+    "date": datetime.datetime.now(tz=datetime.timezone.utc),
+}
+
+posts = collection
+post_id = posts.insert_one(post).inserted_id
+post_id
