5fd630fd70a80518aa3e4bbb3d312184f171dad9
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2024-04-10 22:48:54 +0200

    nuevos jugadores, mínimo 1 gol

--- orange3.py
+++ orange3.py
@@ -1,2 +1,5 @@
-import orange3
-data = orange3.data.Table("docs/data/PlayerStats_22_23/ucl shooting.csv")
+import pandas as pd
+
+df = pd.read_csv(r".\docs\data\clear\strikers\shooting.csv")
+
+df[df["Goals"]>0].to_csv(r".\docs\data\clear\strikers\shooting_goals.csv", index=False)
