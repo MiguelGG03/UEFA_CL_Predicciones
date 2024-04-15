8064eb265a0a47de14e4cc0cfdd0549b8a62afc3
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2024-04-15 16:12:26 +0200

    nuevos datos ya por fin

--- limpieza_portero.py
+++ limpieza_portero.py
@@ -2,7 +2,7 @@ from csv_2_json import *
 
 players = ['Thibaut Courtois']
 
-csv_files = [ ruta("ucl_advanced_goalkeeping.csv"), ruta("ucl_goalkeeper.csv")]
+csv_files = [ ruta_limpia("ucl_advanced_goalkeeping.csv"), ruta_limpia("ucl_goalkeeper.csv")]
 
 for player_name in players:
     adapter = Csv2Json()
