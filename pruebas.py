0a3143c624b989978a3f99ab176917dad30fb4ce
MikylianMbappe <91721552+MiguelGG03@users.noreply.github.com>
2024-04-29 21:44:11 +0200

    entorno y otros archivos eliminados

--- pruebas.py
+++
@@ -1,8 +0,0 @@
-import os
-
-directory = r'docs\data\equipos_jugadores\real_madrid'
-
-for filename in os.listdir(directory):
-        if filename.endswith('.json'):
-            file_path = os.path.join(directory, filename)
-            print(file_path)
