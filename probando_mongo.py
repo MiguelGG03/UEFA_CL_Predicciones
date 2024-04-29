7f01f1eb55eb8a693daa0c5b01e968f2324f7979
MikylianMbappe <91721552+MiguelGG03@users.noreply.github.com>
2024-04-29 21:18:01 +0200

    TODOS LOS DATOS PREPARADOS Y CONECTADOS CON LA BASE DE DATOS

--- probando_mongo.py
+++ probando_mongo.py
@@ -1,6 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from config import *
-import pandas as pd
+import json
+import os
 
 uri = "mongodb+srv://{username}:{password}@cluster0.5noc7o6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0".format(username=username, password=password)
 
@@ -15,5 +16,19 @@ except Exception as e:
 
 db = client.get_database("champions")
 
-collection = db.list_collection_names()
 
+def insert_data_to_database(db,equipo):
+    directory = r'docs\data\equipos_jugadores\{equipo}'.format(equipo=equipo)
+
+    for filename in os.listdir(directory):
+        if filename.endswith('.json'):
+            file_path = os.path.join(directory, filename)
+            print(file_path)
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                db[equipo].insert_one(data)
+
+insert_data_to_database(db,'real_madrid')
+insert_data_to_database(db,'paris_saint_germain')
+insert_data_to_database(db,'borussia_dormund')
+insert_data_to_database(db,'bayern_munich')
