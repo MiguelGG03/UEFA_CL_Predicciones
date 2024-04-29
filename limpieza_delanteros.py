ed77ef35d8aaf76c23d773aae74a5ceb56e6ec0b
MikylianMbappe <91721552+MiguelGG03@users.noreply.github.com>
2024-04-29 17:49:05 +0200

    Refactor code to export player data to JSON format

--- limpieza_delanteros.py
+++ limpieza_delanteros.py
@@ -1,29 +1,16 @@
-import pandas as pd
-import os
+from csv_2_json import *
 
-def no_ucl_in_name(nombre_archivo:str):
-    nombre_final = nombre_archivo.split('_')
-    nombre_final.remove('ucl')
-    nombre_final = '_'.join(nombre_final)
-    return nombre_final
+players = ['Vinicius Júnior','Karim Benzema','Rodrygo Goes',
+           'Kylian Mbappé','Neymar Jr.','Leo Messi',
+           'Sadio Mané', 'Kingsley Coman','Leroy Sané',
+           'Karim Adeyemi','Sébastien Haller','Donyell Malen']
 
+csv_files = [ ruta_limpia("ucl_standard_stats.csv"), ruta_limpia("ucl_miscellanous.csv"), ruta_limpia("ucl_player_playing_time.csv"), ruta_limpia("ucl_possession.csv"),
+            ruta_limpia("ucl_shooting.csv")]
 
-def limpieza(nombre_archivo:str):
-    df = pd.read_csv(r'.\docs\data\PlayerStats_22_23\{}.csv'.format(nombre_archivo))
-
-    equipos = ['Real Madrid','Atletico','Paris S-G','Manchester City','Arsenal','Dortmund','Barcelona','Bayern Munich']
-
-
-    df = df[df['Club'].isin(equipos)]
-
-    nombre_final = no_ucl_in_name(nombre_archivo)
-
-    os.makedirs(r'.\docs\data\clear\strikers', exist_ok=True)
-    
-    df.to_csv(r'.\docs\data\clear\strikers\{}.csv'.format(nombre_final), index=False)
-
-limpieza(input('Nombre del archivo: '))
-
-
-
-
+for player_name in players:
+    adapter = Csv2Json()
+    player_name_ = player_name.replace(' ', '_')
+    player_data = adapter.load_data(player_name, csv_files)
+    save_data_to_json(player_data, r'docs\data\json\{player}.json'.format(player=player_name_))
+    print("Datos exportados exitosamente en el archivo {player}.json".format(player = player_name_))
