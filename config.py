817fadb9bc949569b394eee0d0de856ddb0f7b7e
MikylianMbappe <91721552+MiguelGG03@users.noreply.github.com>
2024-04-29 20:49:01 +0200

    Update player names for Real Madrid, PSG, Bayern Munich, and Borussia Dortmund teams

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ username = 'admin'
 password = 'i9dnviXyDmU14kO8'
 
 
-PLANTILLA_REAL_MADRID = ['Toni Kroos','Luka Modrić','Federico Valverde','Vinicius Júnior','Karim Benzema','Rodrygo','Antonio Rüdiger','Éder Militão','Dani Carvajal','Ferland Mendy','Thibaut Courtois']
+PLANTILLA_REAL_MADRID = ['Toni_Kroos','Luka_Modrić','Federico_Valverde','Vinicius_Júnior','Karim_Benzema','Rodrygo','Antonio_Rüdiger','Éder_Militão','Dani_Carvajal','Ferland_Mendy','Thibaut_Courtois']
 
-PLANTILLA_PSG = ['Marco Verratti','Leandro Paredes','Danilo Pereira','Kylian Mbappé','Neymar','Lionel Messi','Marquinhos','Presnel Kimpembe','Achraf Hakimi','Nuno Mendes','Gianluigi Donnarumma']
+PLANTILLA_PSG = ['Marco_Verratti','Leandro_Paredes','Danilo_Pereira','Kylian_Mbappé','Neymar','Lionel_Messi','Marquinhos','Presnel_Kimpembe','Achraf_Hakimi','Nuno_Mendes','Gianluigi_Donnarumma']
 
-PLANTILLA_BAYERN_MUNICH = ['Joshua Kimmich','Leon Goretzka','Jamal Musiala','Kingsley Coman','Leroy Sané','Sadio Mané','Dayot Upamecano','Alphonso Davies','Benjamin Pavard','Manuel Neuer','Matthijs de Ligt']
+PLANTILLA_BAYERN_MUNICH = ['Joshua_Kimmich','Leon_Goretzka','Jamal_Musiala','Kingsley_Coman','Leroy_Sané','Sadio_Mané','Dayot_Upamecano','Alphonso_Davies','Benjamin_Pavard','Manuel_Neuer','Matthijs_de_Ligt']
 
-PLANTILLA_BORUSSIA_DORTMUND = ['Jude Bellingham','Marco Reus','Emre Can','Karim Adeyemi','Sébastien Haller','Donyell Malen','Mats Hummels','Nico Schlotterbeck','Raphaël Guerreiro','Niklas Süle','Gregor Kobel']
+PLANTILLA_BORUSSIA_DORTMUND = ['Jude_Bellingham','Marco_Reus','Emre_Can','Karim_Adeyemi','Sébastien_Haller','Donyell_Malen','Mats_Hummels','Nico_Schlotterbeck','Raphaël_Guerreiro','Niklas_Süle','Gregor_Kobel']
--- pruebas.py
+++ pruebas.py
@@ -1,5 +1,9 @@
 import pandas as pd
+from config import PLANTILLA_REAL_MADRID
 
 data = pd.read_json('docs/data/equipos/real_madrid.json').to_dict('records')[1]
 
-print(data["Player"])
+for name in PLANTILLA_REAL_MADRID:
+    print(name)
+    print(data[name])
+    print('---')
