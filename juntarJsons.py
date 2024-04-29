b8df5bac9b2bfc6cc87ae768030d0c54200543ee
MikylianMbappe <91721552+MiguelGG03@users.noreply.github.com>
2024-04-29 18:02:06 +0200

    Merge JSON files for Paris Saint-Germain, Bayern Munich, and Borussia Dortmund teams

--- juntarJsons.py
+++ juntarJsons.py
@@ -1,11 +1,13 @@
 import json
-from csv_2_json import save_data_to_json
 from jsonComposite import merge_json_files
 
 # Usar la función
 
 PATH_EQUIPOS_JUGADORES = r'docs\data\equipos_jugadores'
 
-PATH_EQUIPOS = r''
+PATH_EQUIPOS = r'docs\data\equipos'
 
-merge_json_files(PATH_EQUIPOS+r'\real_madrid', 'output.json')
+merge_json_files(PATH_EQUIPOS_JUGADORES+r'\real_madrid', PATH_EQUIPOS+r'real_madrid.json')
+merge_json_files(PATH_EQUIPOS_JUGADORES+r'\bayer_munich', PATH_EQUIPOS+r'bayer_munich.json')
+merge_json_files(PATH_EQUIPOS_JUGADORES+r'\borussia_dormund', PATH_EQUIPOS+r'borussia_dormund.json')
+merge_json_files(PATH_EQUIPOS_JUGADORES+r'\paris_saint_germain', PATH_EQUIPOS+r'paris_saint_germain.json')
