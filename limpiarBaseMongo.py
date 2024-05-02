75f575010b37d3fbbbbd0ffb0faafb70e7962e8e
MikylianMbappe <91721552+MiguelGG03@users.noreply.github.com>
2024-05-02 09:41:43 +0200

    sin unnamed 0

--- limpiarBaseMongo.py
+++ limpiarBaseMongo.py
@@ -1,9 +1,5 @@
 from pymongo.mongo_client import MongoClient
-import sys
-sys.path.append("..")
 from config import *
-
-
 
 
 uri = "mongodb+srv://{username}:{password}@cluster0.5noc7o6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0".format(username=username, password=password)
@@ -19,11 +15,14 @@ except Exception as e:
 
 db = client.get_database("champions")
 
-# Seleccionar la colección
-collection = db['real_madrid']
+collections = ['real_madrid','paris_saint_germain','borussia_dormund','bayern_munich']
 
-# Eliminar el campo 'edad' de todos los documentos
-result = collection.update_many({}, {'$unset': {'Unnamed: 0': 1}})
+for collection_ in collections:
+    # Seleccionar la colección
+    collection = db[collection_]
 
-# Imprimir el número de documentos actualizados
-print('Documentos actualizados:', result.modified_count)
+    # Eliminar el campo 'edad' de todos los documentos
+    result = collection.update_many({}, {'$unset': {'Unnamed: 0': 1}})
+
+    # Imprimir el número de documentos actualizados
+    print('Documentos actualizados:', result.modified_count)
